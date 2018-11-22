9c495f1f6680271aaf928133afb1b46345986d19
olugashi <orenlugashi@gmail.com>
2018-11-22 14:09:59 +0200

    Add nodemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule , } from '@angular/material';
 import { MatButtonModule , } from '@angular/material/button';
 import { CesiumComponent } from './cesium/cesium.component';
-import { DialogCreatePolylineComponentComponent } from './dialog-create-polyline-component/dialog-create-polyline-component.component';
 import { MapToolBarComponentComponent } from './map-tool-bar-component/map-tool-bar-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CesiumComponent
-    DialogCreatePolylineComponentComponent,
-    MapToolBarComponentComponent
+    CesiumComponent,
+    MapToolBarComponentComponent,
   ],
   imports: [
     BrowserModule,
